87e59c556c03837485af51dc622068d46ceb8226
LiamWelfare <liamthewelfare@gmail.com>
2020-12-08 22:28:05 -0500

    Added programmer comments and starter code to method main
    Signed-off-by: LiamWelfare <liamthewelfare@gmail.com>

--- src/Assignment04LiamWelfare.java
+++ src/Assignment04LiamWelfare.java
@@ -1,5 +1,12 @@
 public class Assignment04LiamWelfare {
     public static void main(String[] args) {
-        
+        //declare array with values 2, 3, 5, 7
+        int[] primes = { 2, 3, 5, 7 };
+        //for loop that runs 4 times
+        for(int index = 0; index <= primes.length; index++) {
+            System.out.println(primes[index]);
+        }
+        //print my name
+        System.out.println("Program by Liam Welfare");
     }
 }
